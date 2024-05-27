c8d561a20d3c041607d4fbafe1370dcc9516dd23
arinaapestova <arina.pest@yandex.ru>
2024-05-27 03:15:33 +0300

    Добавлена возможность изменять и сохранять аватар в базе данных

--- DataBase.py
+++ DataBase.py
@@ -1,8 +1,10 @@
 import sqlite3
 import time
 import math
-import re
-from flask import url_for
+
+
+# import re
+# from flask import url_for
 
 
 class DataBase:
@@ -11,7 +13,7 @@ class DataBase:
         self.__cur = db.cursor()
 
     def getMenu(self):
-        sql = '''SELECT * FROM mainmenu'''
+        sql = """SELECT * FROM mainmenu"""
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
@@ -24,15 +26,13 @@ class DataBase:
         try:
             self.__cur.execute(f"SELECT COUNT() as `count` FROM users WHERE email LIKE '{email}'")
             res = self.__cur.fetchone()
-            if res['count'] > 0:
-                print("Пользователь с таким email уже существует")
+            if res["count"] > 0:
                 return False
 
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, ?)", (name, email, hpsw, tm))
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, NULL, ?)", (name, email, hpsw, tm))
             self.__db.commit()
         except sqlite3.Error as e:
-            print("Ошибка добавления пользователя в БД " + str(e))
             return False
 
         return True
@@ -42,25 +42,31 @@ class DataBase:
             self.__cur.execute(f"SELECT * FROM users WHERE id = {user_id} LIMIT 1")
             res = self.__cur.fetchone()
             if not res:
-                print("Пользователь не найден")
                 return False
 
             return res
         except sqlite3.Error as e:
-            print("Ошибка получения данных из БД " + str(e))
-
-        return False
+            return False
 
     def getUserByEmail(self, email):
         try:
             self.__cur.execute(f"SELECT * FROM users WHERE email = '{email}' LIMIT 1")
             res = self.__cur.fetchone()
             if not res:
-                print("Пользователь не найден")
                 return False
 
             return res
         except sqlite3.Error as e:
-            print("Ошибка получения данных из БД " + str(e))
+            return False
 
-        return False
+    def updateUserAvatar(self, avatar, user_id):
+        if not avatar:
+            return False
+
+        try:
+            binary = sqlite3.Binary(avatar)
+            self.__cur.execute(f"UPDATE users SET avatar = ? WHERE id = ?", (binary, user_id))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            return False
+        return True
